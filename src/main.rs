1b761dbac0002b9b43cdd4007fc3375c37d0e4a4
nireo <eemil.lehtikangas1@gmail.com>
2022-05-22 01:18:50 +0300

    added form tests

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use std::net::TcpListener;
+
 use upfi::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:0").expect("failed to bind random port.");
+    run(listener)?.await
 }
